feaa02b442e9ecd62463fb9fe53fa64f5ed4110c
Raphael Honorato <raphaahonorato@gmail.com>
2020-04-16 22:19:04 -0300

    operações aritméticas

---
+++ operadores.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Operações Aritméticas</title>
+</head>
+
+<body>
+    <?php
+    $n1 = 20;
+    $n2 = 10;
+    echo "A soma vale ". ($n1 + $n2);
+    echo "<br/> A subtração vale ". ($n1 - $n2);
+    echo "<br/> A multiplicação vale ". ($n1 * $n2);
+    echo "<br/> A divisão vale ". ($n1 / $n2);
+    echo "<br/> O módulo vale ". ($n1 % $n2);
+    ?>
+</body>
+
+</html>
--- somador.php
+++ somador.php
@@ -1,6 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
+<head>
+    <meta charset="UTF-8">
+    <title>Somador</title>
+</head>
+
+<body>
+    <?php
     $n1 = 1;
     $n2 = 2;
     $s = $n1 + $n2;
-    echo "$n1 + $n2 = $s";
+    echo "A soma entre $n1 e $n2 é igual a $s";
+    ?>
+</body>
+</html>
