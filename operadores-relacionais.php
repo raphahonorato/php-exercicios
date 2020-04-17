544b676fdc92b2e01768eb078b3987ac3cd64646
Raphael Honorato <raphaahonorato@gmail.com>
2020-04-17 13:28:41 -0300

    operadores relacionais

---
+++ operadores-relacionais.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Operadores Relacionais</title>
+    <style>
+        div {
+            font-size: 20px;
+            font-weight: bold;
+            color: white;
+            background-color: black;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <?php
+        echo "";
+        ?>
+    </div>
+</body>
+
+</html>
