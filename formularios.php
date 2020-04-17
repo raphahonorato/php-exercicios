7d308a29ca320a1028ed920ba1b804fb00231040
Raphael Honorato <raphaahonorato@gmail.com>
2020-04-17 15:36:10 -0300

    primeiro formulário

---
+++ formularios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Primeiro Formulário em PHP</title>
+    <style>
+        div {
+            font-size: 25px;
+            font-weight: bold;
+            color: white;
+            background-color: black;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <?php
+        echo "";
+        ?>
+    </div>
+
+</body>
+
+</html>
