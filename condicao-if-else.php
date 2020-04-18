edfcd77a379eb81e00283c1245e70048156a179a
Raphael Honorato <raphaahonorato@gmail.com>
2020-04-18 17:18:39 -0300

    condições aninhadas

---
+++ condicao-if-else.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        $a = isset($_GET["ano"]) ? $_GET["ano"] : 1900;
+        $i = date("Y") - $a;
+        echo "Vc nasceu em $a e terá $i anos <br/>";
+        if ($i < 16) {
+            $tipoVoto = "Não vota";
+        } else {
+            if ($i >= 16 && $i < 18) {
+                $tipoVoto = "Voto Opcional";
+            } else {
+                $tipoVoto = "Voto Obrigatório";
+            }
+        }
+        if ($i > 65) {
+            $tipoVoto = "Velho demais, vota se quiser";
+        }
+        echo "Para essa idade $tipoVoto";
+        ?>
+    </div>
+</body>
+
+</html>
