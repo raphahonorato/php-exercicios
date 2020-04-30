3d47da7e1e38d62c17f91beaa1bf62e7a554b754
Raphael Honorato <raphaahonorato@gmail.com>
2020-04-29 21:33:06 -0300

    estrutura condicional switch

---
+++ condicoes-mult.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../aulaPHP/_css/estilo.css"/>
+    <meta charset="UTF-8" />
+    <title>Condições de Multiplos Casos</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        
+    ?>
+    </div>
+</body>
+
+</html>
