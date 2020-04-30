2eac9d29fd5501f05ac1eb240e2555be23623c03
Raphael Honorato <raphaahonorato@gmail.com>
2020-04-29 22:16:07 -0300

    varios casos

--- condicoes-mult.php
+++ condicoes-mult.php
@@ -2,17 +2,32 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="../aulaPHP/_css/estilo.css"/>
-    <meta charset="UTF-8" />
-    <title>Condições de Multiplos Casos</title>
+  <link rel="stylesheet" href="../aulaPHP/_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title>Condições de Multiplos Casos</title>
 </head>
 
 <body>
-    <div>
-        <?php
-        
+  <div>
+    <?php
+    $n = isset($_GET["num"]) ? $_GET["num"] : 0;
+    $o = isset($_GET["oper"]) ? $_GET["oper"] : 1;
+    switch ($o) {
+      case 1:
+        $r = $n * 2;
+        break;
+      case 2:
+        $r = $n ^ 3;
+        break;
+      case 3:
+        $r = sqrt($n);
+        break;
+    }
+
+    echo ("O resultado é $r");
     ?>
-    </div>
+    <a href="condicoes-mult.html" class="botao">Voltar</a>
+  </div>
 </body>
 
 </html>
