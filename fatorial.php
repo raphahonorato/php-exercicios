081d9fd3a4a198676e975f46d67751ad9f4fa0e4
Raphael Honorato <raphaahonorato@gmail.com>
2020-05-04 23:02:58 -0300

    uma hora vai :(

--- fatorial.php
+++ fatorial.php
@@ -9,11 +9,41 @@
 
 <body>
     <div>
-        <form method="get" action="contador.php">
-            
-            <a href="fatorial.html" class="botao">Enviar</a>
+        <?php
+        $n = $_GET['val'];
+        $n2 = $n1;
+        $tot = 0;
+
+        while ($n >= 1) {
+            $n2 = $n2 - 1;
+            $tot = $n2 * $n2;
+            $n--;
+            echo "$tot <br>";
+        }
+        ?>
         </form>
     </div>
 </body>
 
+//
+
+<?php
+
+    
+    
+        $v = isset($_GET["val"]) ? $_GET["val"] : 1;
+        echo "<h1>Calculando o fatorial de $v </h1>";
+        $c = $v;
+        $fat = 1;
+        do {
+            $fat= $fat * $c;
+            $c-= 1;
+        }while ($c >=1);
+        echo $c;
+        ?>
+
+        <a href="fatorial.html" class="botao">Voltar</a>
+*/
+ ?>       
+//
 </html>
