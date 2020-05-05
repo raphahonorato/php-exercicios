0726802e7a15832375ca9b78e3bd7e6b930675da
Raphael Honorato <raphaahonorato@gmail.com>
2020-05-04 21:58:29 -0300

    Do e While

---
+++ Untitled-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../aulaPHP/_css/estilo.css" />
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <div>
+        <form method="get" action="mult-form.php">
+            <?php
+            
+            ?>
+            <a href="mult-form.php" class="botao">Enviar</a>
+        </form>
+    </div>
+</body>
+
+</html>
