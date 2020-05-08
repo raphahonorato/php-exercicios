87af78b9708bc57d4d6554e2dd505c9017458370
Raphael Honorato <raphaahonorato@gmail.com>
2020-05-08 02:42:45 -0300

    tabuada

--- tabuada.php
+++ tabuada.php
@@ -11,15 +11,12 @@
     <div>
         
             <?php
-            $v = isset($_GET["val"]) ? $_GET["val"] : 1;
-            echo "<h1>Calculando o fatorial de $v</h1>";
-            $c = $v ;
-            $fat = 1;
-            do {
-                $fat = $fat * $c;
-                $c--;
-            }while($c >= 1);
-            echo "$v !&nbsp;&nbsp;= $fat &nbsp;&nbsp;&nbsp;&nbsp;";
+            $n = isset($_GET["val"]) ? $_GET["val"] : 1;
+            echo "<h1>Mostrando a Tabuada de $n </h1>";
+	   for ($c = 1; $c <= 10; $c++) {
+		 $r = $n * $c;
+		 echo "$n x $c = $r <br/>";
+	   }
             ?>
             <input type="button" value="Atualizar" onClick="history.go(0)" class="botao">
             <input type="button" value="Voltar" onClick="history.go(-1)" class="botao">
