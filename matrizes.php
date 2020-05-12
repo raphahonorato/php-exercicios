f0b9bdf9410c3e616d5004ba64fd617e7fb81293
Raphael Honorato <raphaahonorato@gmail.com>
2020-05-12 17:01:49 -0300

    atribuições de vetores

--- matrizes.php
+++ matrizes.php
@@ -11,12 +11,22 @@
     <div>
         <pre>
         <?php
-        $m = array(
+        /*$m = array(
             array(2, 3),
             array(4, 5),
             array(6, 7));
             $m[2][0] = $m[0][1];
         print_r($m);
+            */
+        $v = array("R","A","P","H","A","E","L");
+        /*foreach($v as $valor){
+            echo$valor;
+        }
+            print_r($v);
+            var_dump($v);*/
+            echo count($v);//quantidade de elementos contidos no vetor
+        
+
         ?>
         </pre>
         <input type="button" value="Atualizar" onClick="history.go(0)" class="botao">
