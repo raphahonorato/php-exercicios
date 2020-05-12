78a5cb2467de61020d09aed5dc9d9ed0efbba1b5
Raphael Honorato <raphaahonorato@gmail.com>
2020-05-12 16:13:02 -0300

    começo de matrizes

---
+++ matrizes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../aulaPHP/_css/estilo.css" />
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <div>
+
+        <pre>
+        <table>
+        <?php
+       
+        ?>
+        </table>
+        </pre>
+        <input type="button" value="Atualizar" onClick="history.go(0)" class="botao">
+    </div>
+</body>
+
+</html>
