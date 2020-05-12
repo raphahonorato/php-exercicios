ec17778dff14156301b61dafb4fd6a7d9f334498
Raphael Honorato <raphaahonorato@gmail.com>
2020-05-12 13:22:12 -0300

    code para letras

--- strings-3.php
+++ strings-3.php
@@ -21,9 +21,15 @@
 
         $nome = "Raphael";
         /*$vetor = explode(" ",$nome);
-        print_r($vetor); */
+        print_r($vetor); 
         $vetor = str_split($nome);
-        print_r($vetor);
+        print_r($vetor);*/
+        $vetor[1] = "Raphael";
+        $vetor[2] = "Honorato";
+        $vetor[3] = "e";
+        $vetor[4] = "Silva";
+        $nome = implode("#", $vetor);//$nome = join("#", $vetor);  --> funciona da mesma forma que o implode
+        print($nome);
         ?>
 
         <input type="button" value="Atualizar" onClick="history.go(0)" class="botao">
---
+++ strings-code.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../aulaPHP/_css/estilo.css" />
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <div>
+        <?php
+       /*$letra = chr(100);
+       echo "100 é o código de $letra";*/
+
+               
+        ?>
+
+        <input type="button" value="Atualizar" onClick="history.go(0)" class="botao">
+    </div>
+</body>
+
+</html>
